56d977ea79a3d3c83429d0007a903c4529c4e0f3
Blake LeBlanc <blake.leblanc@guernsey.us>
2024-12-20 12:42:07 -0600

    Update wezterm; Disable azure cli related

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,13 +1,35 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
+-- color schemes {{{
+-- light
+-- config.color_scheme = "aikofog (terminal.sexy)"
+config.color_scheme = "Ashes (light) (terminal.sexy)"
+-- config.color_scheme = 'AtomOneLight'
+-- config.color_scheme = 'ayu_light'
+-- config.color_scheme = "Belafonte Day"
+-- config.color_scheme = 'BlulocoLight'
+-- config.color_scheme = 'Breadog (Gogh)'
+-- config.color_scheme = 'Catppuccin Latte'
+-- config.color_scheme = 'Catppuccin Latte (Gogh)'
+
+-- dark
+-- config.color_scheme = 'Arthur (Gogh)'
+
+-- }}}
+-- font {{{
 config.font = wezterm.font("JetBrainsMonoNL Nerd Font")
-config.font_size = 10
+config.font_size = 11
 config.line_height = 1.75
 
-config.enable_tab_bar = false
+-- }}}
+-- window decorations {{{
 -- config.window_decorations = "TITLE | RESIZE"
 config.window_decorations = "RESIZE"
-config.window_background_opacity = 0.9
+
+-- }}}
+
+config.enable_tab_bar = false
+-- config.window_background_opacity = 0.9
 
 return config
