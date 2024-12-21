90267115e9c1ab450b6cce8aeb6e5097879ede05
Blake LeBlanc <blake.leblanc@guernsey.us>
2024-12-21 08:48:35 -0600

    Update wezterm config, add cursor settings

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -17,8 +17,22 @@ config.color_scheme = "Catppuccin Latte"
 -- config.color_scheme = "Breadog (Gogh)"
 
 -- }}}
+-- cursor {{{
+config.animation_fps = 60
+-- config.cursor_blink_ease_in = "EaseOut"
+-- config.cursor_blink_ease_out = "EaseOut"
+config.cursor_blink_ease_in = "Constant"
+config.cursor_blink_ease_out = "Constant"
+-- config.default_cursor_style = "SteadyBlock"
+-- config.default_cursor_style = "SteadyUnderline"
+config.default_cursor_style = "BlinkingBlock"
+-- config.default_cursor_style = "BlinkingUnderline"
+config.cursor_blink_rate = 600
+
+-- }}}
 -- font {{{
-config.font = wezterm.font("JetBrainsMonoNL Nerd Font")
+-- config.font = wezterm.font("JetBrainsMonoNL Nerd Font")
+config.font = wezterm.font("JetBrains Mono")
 config.font_size = 11
 config.line_height = 1.75
 
@@ -32,5 +46,6 @@ config.window_decorations = "RESIZE"
 config.enable_tab_bar = false
 -- config.window_background_opacity = 0.9
 config.default_prog = { "pwsh" }
+config.max_fps = 120
 
 return config
