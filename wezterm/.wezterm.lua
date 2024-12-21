c69360af42f0d332e5a8db94a61247f8cf47ca4e
Blake LeBlanc <blake.leblanc@guernsey.us>
2024-12-21 09:31:51 -0600

    Add startup sizing

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,4 +1,5 @@
 local wezterm = require("wezterm")
+local mux = wezterm.mux
 local config = wezterm.config_builder()
 
 -- color schemes {{{
@@ -35,6 +36,26 @@ config.cursor_blink_rate = 600
 config.font = wezterm.font("JetBrains Mono")
 config.font_size = 11
 config.line_height = 1.75
+
+-- }}}
+-- startup{{{
+wezterm.on("gui-startup", function(cmd)
+	local tab, pane, window = mux.spawn_window(cmd or {})
+	window:gui_window():maximize()
+
+	-- Attempting to center vertically and horizontally, but not working properly...
+	-- local screen = wezterm.gui.screens().active
+	-- local ratio = 0.7
+	-- local width, height = screen.width * ratio, screen.height * ratio
+	-- local tab, pane, window = wezterm.mux.spawn_window({
+	-- 	positon = {
+	-- 		x = (screen.width - width) / 2,
+	-- 		y = (screen.height - height) / 2,
+	-- 		origin = "ActiveScreen",
+	-- 	},
+	-- })
+	-- window:gui_window():set_inner_size(width, height)
+end)
 
 -- }}}
 -- window decorations {{{
