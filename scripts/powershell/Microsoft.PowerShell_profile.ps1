b33947ccea2d5f5c69e9ddc5aa19795525b7e06e
Blake LeBlanc <blake.leblanc1@gmail.com>
2026-06-23 05:41:16 -0500

    Add dltmo

--- scripts/powershell/Microsoft.PowerShell_profile.ps1
+++ scripts/powershell/Microsoft.PowerShell_profile.ps1
@@ -209,6 +209,20 @@ function dlm {
     yt-dlp --config-location "$env:APPDATA\yt-dlp\music.conf" -a "$HOME\yt-dlp\music.txt" @args
 }
 
+function dltmo {
+    $downloadDir = "$env:USERPROFILE\Downloads\yt-dlp\music"
+
+    yt-dlp --config-location "$env:APPDATA\yt-dlp\music.conf" @args
+
+    Get-ChildItem -Path $downloadDir -Recurse -Filter "*.mp3" | ForEach-Object {
+        freaccmd -e opus -d $_.DirectoryName -- --vbr --bitrate 96 $_.FullName
+    }
+
+    Get-ChildItem -Path $downloadDir -Recurse -Filter "*.mp3" | ForEach-Object {
+        Remove-ItemToRecycleBin $_.FullName
+    }
+}
+
 function dlt {
     yt-dlp --config-location "$env:APPDATA\yt-dlp\talks.conf" @args
 }
@@ -218,15 +232,15 @@ function dlv {
 }
 
 function dlto {
-    $talksDir = "$env:USERPROFILE\Downloads\yt-dlp\talks"
+    $downloadDir = "$env:USERPROFILE\Downloads\yt-dlp\talks"
 
     yt-dlp --config-location "$env:APPDATA\yt-dlp\talks.conf" @args
 
-    Get-ChildItem -Path $talksDir -Recurse -Filter "*.mp3" | ForEach-Object {
+    Get-ChildItem -Path $downloadDir -Recurse -Filter "*.mp3" | ForEach-Object {
         freaccmd -e opus -d $_.DirectoryName -- --vbr --bitrate 8 $_.FullName
     }
 
-    Get-ChildItem -Path $talksDir -Recurse -Filter "*.mp3" | ForEach-Object {
+    Get-ChildItem -Path $downloadDir -Recurse -Filter "*.mp3" | ForEach-Object {
         Remove-ItemToRecycleBin $_.FullName
     }
 }
